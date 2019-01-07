9c1ecfbbad704019e26e872867037f6eca270067
CrazyAppel <johoapple@gmail.com>
2019-01-07 23:09:20 +0100

    Test_commit

---
+++ GameWorld/Managers/AnimationManager.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameWorld.Models;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameWorld.Managers
+{
+    public class AnimationManager
+    {
+        private Animations _animation;
+        private float _timer;
+        public Vector2 Position { get; set; }
+
+        public AnimationManager(Animations animation)
+        {
+            _animation = animation;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_animation.Texture, Position, new Rectangle(_animation.CurrentFrame * _animation.FrameWidth, 0, _animation.FrameWidth, _animation.FrameHeight), Color.White);
+        }
+
+        public void Play(Animations animation)
+        {
+            if (_animation == animation)
+                return;
+
+            _animation = animation;
+            _animation.CurrentFrame = 0;
+
+            _timer = 0;
+        }
+        public void Stop()
+        {
+            _timer = 0f;
+
+            _animation.CurrentFrame = 0;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (_timer > _animation.FrameSpeed)
+            {
+                _timer = 0f;
+
+                _animation.CurrentFrame++;
+
+                if (_animation.CurrentFrame >= _animation.FrameCount)
+                    _animation.CurrentFrame = 0;
+            }
+        }
+    }
+}

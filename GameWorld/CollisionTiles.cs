3642dd49a22eeb4091a16afc13f35dae4f7585a8
CrazyAppel <johoapple@gmail.com>
2019-01-08 00:32:56 +0100

    Update CollisionTiles.cs

--- GameWorld/CollisionTiles.cs
+++ GameWorld/CollisionTiles.cs
@@ -16,10 +16,18 @@ namespace GameWorld
             if (isDeadly == true)
             {
                 this.isDeadly = true;
+                texture = Content.Load<Texture2D>("Tile" + tileId);
+                //newRectangle.Size = newRectangle.Size - (new Point(64, 40));
+                newRectangle.Height = 55;
+                newRectangle.Width = 64;
+                newRectangle.Location = new Point(newRectangle.Location.X, newRectangle.Location.Y + (64-55));
+                this.Rectangle = newRectangle;
             }
-            
-            texture = Content.Load<Texture2D>("Tile" + tileId);
-            this.Rectangle = newRectangle;
+            else
+            {
+                texture = Content.Load<Texture2D>("Tile" + tileId);
+                this.Rectangle = newRectangle;
+            }
         }
     }
 }
